9b4fed271896480efb1d1a4478b010911647e188
Christian Kaestner <kaestner@cs.cs.cmu.edu>
2016-01-17 10:08:46 -0500

    design notes on scheduling

--- src/main/java/edu/cmu/cs/vbc/Tmp.java
+++ src/main/java/edu/cmu/cs/vbc/Tmp.java
@@ -9,6 +9,19 @@ import edu.cmu.cs.varex.V;
  */
 public class Tmp {
   void test() {
+//    l1:
+//    System.out.println("1");
+//    l2:
+//    System.out.println("2");
+//    l3:
+//    System.out.println("3");
+//    l4:
+//    System.out.println("4");
+//    l5:
+//    System.out.println("5");
+
+
+
     System.out.println(V.one(18));
     FeatureExpr a = FeatureExprFactory.True();
     FeatureExpr b = FeatureExprFactory.createDefinedExternal("B");
