944e90e83666036f1c34294b5efca152ece67df2
Christian Kaestner <kaestner@cs.cs.cmu.edu>
2016-02-28 12:23:36 -0500

    fix simple comparison bug

--- src/main/scala/loader/Loader.scala
+++ src/main/scala/loader/Loader.scala
@@ -35,11 +35,11 @@ class Loader {
         if (cl.methods == null) Nil else cl.methods.map(adaptMethod(cl.name, _)).toList,
         if (cl.sourceDebug != null && cl.sourceFile != null) Some(cl.sourceFile, cl.sourceDebug) else None,
         if (cl.outerClass != null) Some(cl.outerClass, cl.outerMethod, cl.outerMethodDesc) else None,
-        if (cl.visibleAnnotations != null) Nil else cl.visibleAnnotations.toList,
-        if (cl.invisibleAnnotations != null) Nil else cl.invisibleAnnotations.toList,
-        if (cl.visibleTypeAnnotations != null) Nil else cl.visibleTypeAnnotations.toList,
-        if (cl.invisibleTypeAnnotations != null) Nil else cl.invisibleTypeAnnotations.toList,
-        if (cl.attrs != null) Nil else cl.attrs.toList,
+        if (cl.visibleAnnotations == null) Nil else cl.visibleAnnotations.toList,
+        if (cl.invisibleAnnotations == null) Nil else cl.invisibleAnnotations.toList,
+        if (cl.visibleTypeAnnotations == null) Nil else cl.visibleTypeAnnotations.toList,
+        if (cl.invisibleTypeAnnotations == null) Nil else cl.invisibleTypeAnnotations.toList,
+        if (cl.attrs == null) Nil else cl.attrs.toList,
         if (cl.innerClasses == null) Nil else cl.innerClasses.map(adaptInnerClass).toList
     )
 
@@ -93,11 +93,11 @@ class Loader {
         field.desc,
         field.signature,
         field.value,
-        if (field.visibleAnnotations != null) Nil else field.visibleAnnotations.toList,
-        if (field.invisibleAnnotations != null) Nil else field.invisibleAnnotations.toList,
-        if (field.visibleTypeAnnotations != null) Nil else field.visibleTypeAnnotations.toList,
-        if (field.invisibleTypeAnnotations != null) Nil else field.invisibleTypeAnnotations.toList,
-        if (field.attrs != null) Nil else field.attrs.toList
+        if (field.visibleAnnotations == null) Nil else field.visibleAnnotations.toList,
+        if (field.invisibleAnnotations == null) Nil else field.invisibleAnnotations.toList,
+        if (field.visibleTypeAnnotations == null) Nil else field.visibleTypeAnnotations.toList,
+        if (field.invisibleTypeAnnotations == null) Nil else field.invisibleTypeAnnotations.toList,
+        if (field.attrs == null) Nil else field.attrs.toList
     )
 
     def adaptInnerClass(m: InnerClassNode): VBCInnerClassNode = ???
