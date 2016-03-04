037b29f4192cf22a78366ee66e7c1566f9b9ab76
Christian Kaestner <kaestner@cs.cs.cmu.edu>
2016-03-03 20:57:07 -0500

    add benchmarking infrastructure

--- src/test/scala/DiffLaunchTestInfrastructure.scala
+++ src/test/scala/DiffLaunchTestInfrastructure.scala
@@ -16,14 +16,11 @@ trait DiffLaunchTestInfrastructure {
       *
       * also change the way that conditional fields are initialized
       */
-    def instrumentMethod(method: VBCMethodNode): VBCMethodNode = method.copy(body = CFG(method.body.blocks.map(instrumentBlock)))
+    def instrumentMethod(method: VBCMethodNode): VBCMethodNode = instrumentCustomInit(method.copy(body = CFG(method.body.blocks.map(instrumentBlock))))
 
-    def instrumentBlock(block: Block): Block = {
-        val v = Block((
+    def instrumentBlock(block: Block): Block =
+        Block((
             for (instr <- block.instr) yield instr match {
-                //replace initialization of conditional fields
-                case InstrINIT_CONDITIONAL_FIELDS() => List(TraceInstr_ConfigInit())
-                //extra instrumentation
                 case InstrINVOKEVIRTUAL("java/io/PrintStream", "println", "(Ljava/lang/String;)V", _) => List(TraceInstr_Print(), instr)
                 case InstrINVOKEVIRTUAL("java/io/PrintStream", "println", "(Ljava/lang/Object;)V", _) => List(TraceInstr_Print(), instr)
                 case InstrINVOKEVIRTUAL(owner, name, desc, _) => List(TraceInstr_S("INVK_VIRT: " + owner + ";" + name + ";" + desc), instr)
@@ -34,8 +31,21 @@ trait DiffLaunchTestInfrastructure {
             }
             ).flatten: _*
         )
-        v
-    }
+
+
+    def instrumentCustomInit(method: VBCMethodNode): VBCMethodNode = method.copy(body = CFG(method.body.blocks.map(instrumentBlock)))
+
+    def instrumentCustomInit(block: Block): Block =
+        Block(
+            (for (instr <- block.instr) yield instr match {
+                //replace initialization of conditional fields
+                case InstrINIT_CONDITIONAL_FIELDS() => TraceInstr_ConfigInit()
+
+                case instr => instr
+            }): _*
+        )
+
+
 
 
     def testMain(classname: String): Unit = {
@@ -45,9 +55,9 @@ trait DiffLaunchTestInfrastructure {
         //test instrumented version, executed variationally
         TestTraceOutput.trace = Nil
         TraceConfig.options = Set()
-        val loader: VBCClassLoader = new VBCClassLoader(this.getClass.getClassLoader, true, instrumentMethod)
-        val cls: Class[_] = loader.loadClass(classname)
-        VBCLauncher.invokeMain(cls, new Array[String](0))
+        val vloader: VBCClassLoader = new VBCClassLoader(this.getClass.getClassLoader, true, instrumentMethod)
+        val vcls: Class[_] = vloader.loadClass(classname)
+        VBCLauncher.invokeMain(vcls, new Array[String](0))
 
         val vtrace = TestTraceOutput.trace
         val usedOptions = TraceConfig.options.map(_.feature)
@@ -55,13 +65,13 @@ trait DiffLaunchTestInfrastructure {
         println("Used Options: " + TraceConfig.options.mkString(", "))
 
 
+        val loader: VBCClassLoader = new VBCClassLoader(this.getClass.getClassLoader, false, instrumentMethod)
+        val cls: Class[_] = loader.loadClass(classname)
         //run against brute force instrumented execution and compare traces
         for ((sel, desel) <- explode(usedOptions.toList)) {
             println("executing config [" + sel.mkString(", ") + "]")
             TestTraceOutput.trace = Nil
             TraceConfig.config = configToMap((sel, desel))
-            val loader: VBCClassLoader = new VBCClassLoader(this.getClass.getClassLoader, false, instrumentMethod)
-            val cls: Class[_] = loader.loadClass(classname)
             VBCLauncher.invokeMain(cls, new Array[String](0))
             val atrace = TestTraceOutput.trace
 
@@ -70,7 +80,12 @@ trait DiffLaunchTestInfrastructure {
             compareTraces(sel, atrace.map(_._2).reverse, filteredvtrace.map(_._2).reverse)
         }
 
-        //TODO run benchmark
+        //run benchmark (without instrumentation)
+        val vbenchmarkloader: VBCClassLoader = new VBCClassLoader(this.getClass.getClassLoader, true, instrumentCustomInit)
+        val vbenchmarkcls: Class[_] = vbenchmarkloader.loadClass(classname)
+        val benchmarkloader: VBCClassLoader = new VBCClassLoader(this.getClass.getClassLoader, false, instrumentCustomInit)
+        val benchmarkcls: Class[_] = benchmarkloader.loadClass(classname)
+        benchmark(classname, vbenchmarkcls, benchmarkcls, usedOptions)
     }
 
     type Feature = String
@@ -98,5 +113,45 @@ trait DiffLaunchTestInfrastructure {
         assert(expected == actual,
             "mismatch between plain execution and variational execution in config [" + ctx.mkString(", ") + "]:\n" +
                 "EXPECTED (plain execution):\n" + expected.mkString("\n") + "\nFOUND (variational execution in [" + ctx.mkString(", ") + "]):\n" + actual.mkString("\n") + "\n")
+
+
+    def benchmark(classname: String, testVClass: Class[_], testClass: Class[_], configOptions: Set[String]): Unit = {
+        import org.scalameter._
+
+        //measure V execution
+        val vtime = config(
+            Key.exec.benchRuns -> 20
+        ) withWarmer {
+            new Warmer.Default
+        } setUp { _ =>
+            TestTraceOutput.trace = Nil
+            TraceConfig.config = Map()
+        } measure {
+            VBCLauncher.invokeMain(testVClass, new Array[String](0))
+        }
+        //        println(s"Total time V: $time")
+
+
+        //measure brute-force execution
+        val configs = explode(configOptions.toList)
+        val bftimes = for ((sel, desel) <- configs)
+            yield config(
+                Key.exec.benchRuns -> 20
+            ) withWarmer {
+                new Warmer.Default
+            } setUp { _ =>
+                TestTraceOutput.trace = Nil
+                TraceConfig.config = configToMap((sel, desel))
+            } measure {
+                VBCLauncher.invokeMain(testVClass, new Array[String](0))
+            }
+
+
+        val avgTime = bftimes.map(_.value).sum / bftimes.size
+        println(s"VExecution time [$classname]: " + vtime)
+        println(s"Execution time [$classname]: " + avgTime + bftimes.mkString(" (", ",", ")"))
+        println(s"Slowdown [$classname]: " + vtime.value / avgTime)
+
+    }
 
 }
