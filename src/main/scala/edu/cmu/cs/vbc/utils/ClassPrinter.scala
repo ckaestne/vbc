a1faffb18de4f0cf01cacd27bb0fdf6f8cf11d79
chupanw <chupanw@cs.cmu.edu>
2016-03-09 21:07:55 -0500

    Removed redundant MethodAnalyzer

--- src/main/scala/edu/cmu/cs/vbc/utils/ClassPrinter.scala
+++ src/main/scala/edu/cmu/cs/vbc/utils/ClassPrinter.scala
@@ -3,7 +3,7 @@ package edu.cmu.cs.vbc.utils
 import java.io.PrintWriter
 
 import org.objectweb.asm.ClassReader
-import org.objectweb.asm.util.{ASMifier, TraceClassVisitor}
+import org.objectweb.asm.util.TraceClassVisitor
 
 /**
   * @author chupanw
@@ -11,6 +11,6 @@ import org.objectweb.asm.util.{ASMifier, TraceClassVisitor}
 object ClassPrinter extends App{
 
   val cr = new ClassReader(args(0))
-  cr.accept(new TraceClassVisitor(null, new ASMifier(), new PrintWriter(System.out)), 0)
+  cr.accept(new TraceClassVisitor(null, new PrintWriter(System.out)), 0)
 
 }
