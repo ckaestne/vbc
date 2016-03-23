7ec68ff57e35fd1264b3419613abca7aa77ab184
chupanw <chupanw@cs.cmu.edu>
2016-03-22 22:56:42 -0400

    Use 2-space indentation instead of 4-space

--- src/main/scala/edu/cmu/cs/vbc/vbytecode/instructions/StackInstructions.scala
+++ src/main/scala/edu/cmu/cs/vbc/vbytecode/instructions/StackInstructions.scala
@@ -9,14 +9,14 @@ import org.objectweb.asm.Opcodes._
   * DUP instruction
   */
 case class InstrDUP() extends Instruction {
-    override def toByteCode(mv: MethodVisitor, env: MethodEnv, block: Block): Unit = {
-        mv.visitInsn(DUP)
-    }
+  override def toByteCode(mv: MethodVisitor, env: MethodEnv, block: Block): Unit = {
+    mv.visitInsn(DUP)
+  }
 
-    override def toVByteCode(mv: MethodVisitor, env: VMethodEnv, block: Block): Unit = {
-        //TODO, when applied to LONG, use the int one instead of the 2byte one
-        mv.visitInsn(DUP)
-    }
+  override def toVByteCode(mv: MethodVisitor, env: VMethodEnv, block: Block): Unit = {
+    //TODO, when applied to LONG, use the int one instead of the 2byte one
+    mv.visitInsn(DUP)
+  }
 }
 
 
@@ -24,13 +24,13 @@ case class InstrDUP() extends Instruction {
   * POP instruction
   */
 case class InstrPOP() extends Instruction {
-    override def toByteCode(mv: MethodVisitor, env: MethodEnv, block: Block): Unit = {
-        mv.visitInsn(POP)
-    }
+  override def toByteCode(mv: MethodVisitor, env: MethodEnv, block: Block): Unit = {
+    mv.visitInsn(POP)
+  }
 
-    override def toVByteCode(mv: MethodVisitor, env: VMethodEnv, block: Block): Unit = {
-        mv.visitInsn(POP)
-    }
+  override def toVByteCode(mv: MethodVisitor, env: VMethodEnv, block: Block): Unit = {
+    mv.visitInsn(POP)
+  }
 }
 
 
@@ -40,19 +40,19 @@ case class InstrPOP() extends Instruction {
   * @param value
   */
 case class InstrBIPUSH(value: Int) extends Instruction {
-    override def toByteCode(mv: MethodVisitor, env: MethodEnv, block: Block): Unit = {
-        mv.visitIntInsn(BIPUSH, value)
-    }
+  override def toByteCode(mv: MethodVisitor, env: MethodEnv, block: Block): Unit = {
+    mv.visitIntInsn(BIPUSH, value)
+  }
 
-    override def toVByteCode(mv: MethodVisitor, env: VMethodEnv, block: Block): Unit = {
-        if (env.shouldLiftInstr(this)) {
-            pushConstant(mv, value)
-            mv.visitMethodInsn(INVOKESTATIC, "java/lang/Integer", "valueOf", "(I)Ljava/lang/Integer;", false)
-            callVCreateOne(mv)
-        }
-        else
-            toByteCode(mv, env, block)
+  override def toVByteCode(mv: MethodVisitor, env: VMethodEnv, block: Block): Unit = {
+    if (env.shouldLiftInstr(this)) {
+      pushConstant(mv, value)
+      mv.visitMethodInsn(INVOKESTATIC, "java/lang/Integer", "valueOf", "(I)Ljava/lang/Integer;", false)
+      callVCreateOne(mv)
     }
+    else
+      toByteCode(mv, env, block)
+  }
 }
 
 
@@ -62,17 +62,17 @@ case class InstrBIPUSH(value: Int) extends Instruction {
   * @param value
   */
 case class InstrSIPUSH(value: Int) extends Instruction {
-    override def toByteCode(mv: MethodVisitor, env: MethodEnv, block: Block): Unit = {
-        mv.visitIntInsn(SIPUSH, value)
-    }
+  override def toByteCode(mv: MethodVisitor, env: MethodEnv, block: Block): Unit = {
+    mv.visitIntInsn(SIPUSH, value)
+  }
 
-    override def toVByteCode(mv: MethodVisitor, env: VMethodEnv, block: Block): Unit = {
-        if (env.shouldLiftInstr(this)) {
-            mv.visitIntInsn(SIPUSH, value)
-            mv.visitMethodInsn(INVOKESTATIC, "java/lang/Integer", "valueOf", genSign("I", primitiveToObjectType("I")), false)
-            callVCreateOne(mv)
-        }
-        else
-            toByteCode(mv, env, block)
+  override def toVByteCode(mv: MethodVisitor, env: VMethodEnv, block: Block): Unit = {
+    if (env.shouldLiftInstr(this)) {
+      mv.visitIntInsn(SIPUSH, value)
+      mv.visitMethodInsn(INVOKESTATIC, "java/lang/Integer", "valueOf", genSign("I", primitiveToObjectType("I")), false)
+      callVCreateOne(mv)
     }
+    else
+      toByteCode(mv, env, block)
+  }
 }
