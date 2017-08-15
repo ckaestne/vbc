8593ff45a3cf6ad27cbd671762309f5e7b7f4e2a
chupanw <chupanw@cs.cmu.edu>
2017-08-15 13:22:39 -0400

    Add new LDC type.

--- src/main/scala/edu/cmu/cs/vbc/vbytecode/instructions/ConstantInstructions.scala
+++ src/main/scala/edu/cmu/cs/vbc/vbytecode/instructions/ConstantInstructions.scala
@@ -74,6 +74,7 @@ case class InstrLDC(o: Object) extends Instruction {
       o match {
         case s: String => wrapString(mv)
         case i: Integer =>  // do nothing
+        case t: Type => // do nothing
         case _ => throw new UnsupportedOperationException("Unsupported LDC type")
       }
     }
