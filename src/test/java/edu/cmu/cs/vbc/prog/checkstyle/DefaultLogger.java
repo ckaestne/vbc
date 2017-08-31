46eb22cf694c1d4adeeffec4a6ebe1b585ad95ab
chupanw <chupanw@cs.cmu.edu>
2017-08-31 15:38:41 -0400

    Disable the synchronized part.

--- src/test/java/edu/cmu/cs/vbc/prog/checkstyle/DefaultLogger.java
+++ src/test/java/edu/cmu/cs/vbc/prog/checkstyle/DefaultLogger.java
@@ -121,10 +121,10 @@ public class DefaultLogger
     @Override
     public void addException(AuditEvent evt, Throwable throwable)
     {
-        synchronized (errorWriter) {
+//        synchronized (errorWriter) {
             errorWriter.println("Error auditing " + evt.getFileName());
             throwable.printStackTrace(errorWriter);
-        }
+//        }
     }
 
     /** {@inheritDoc} */
