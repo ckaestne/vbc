7acf0a6a055114eb24624678eeae62522af1073d
chupanw <chupanw@cs.cmu.edu>
2017-11-07 17:58:07 -0500

    Create a client to visit Jetty server in a loop

---
+++ src/test/java/edu/cmu/cs/vbc/prog/jetty/SimpleClient.java
@@ -0,0 +1,38 @@
+package edu.cmu.cs.vbc.prog.jetty;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ConnectException;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * This class should be executed without VarexC
+ *
+ * @author: chupanw
+ */
+public class SimpleClient {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        URL localhost = new URL("http://localhost:8080");
+        boolean connected = false;
+        BufferedReader in;
+        while (!connected) {
+            connected = true;
+            try {
+                URLConnection connection = localhost.openConnection();
+                in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line;
+                while ((line = in.readLine()) != null) {
+                    System.out.println(line);
+                }
+                in.close();
+            } catch (ConnectException e) {
+                connected = false;
+                Thread.sleep(200);
+                System.out.println("failed..");
+            }
+        }
+    }
+}
