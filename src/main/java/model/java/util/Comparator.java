133d38221d2a296196bf4e98197b7e6327f4526c
Christian Kaestner <kaestner@cs.cmu.edu>
2017-11-23 12:54:06 -0500

    fix assertion
    was throwing exceptions because it matched the model.Integer class rather than the original one

--- src/main/java/model/java/util/Comparator.java
+++ src/main/java/model/java/util/Comparator.java
@@ -15,8 +15,11 @@ public interface Comparator {
         FeatureExpr ctx = Contexts.model_java_util_Comparator_compare;
         V<?> compareResult = compare__Ljava_lang_Object_Ljava_lang_Object__I(V.one(ctx, a), V.one(ctx, b), ctx);
         V<?> selected = compareResult.select(ctx);
-        assert selected.getOne() instanceof Integer : "compare returns non-Integer";
+        Object val = selected.getOne();
+        assert val != null : "compare uses null";
+        assert val instanceof java.lang.Integer : "compare returns non-Integer: " + val + " (" + val.getClass().getCanonicalName() + ")";
         return ((java.lang.Integer) selected.getOne());
     }
+
     V<?> compare__Ljava_lang_Object_Ljava_lang_Object__I(V a, V b, FeatureExpr fe);
 }
